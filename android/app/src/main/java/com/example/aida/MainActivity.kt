0d0a4374d1ed5cc86e388cddd0ac75489d3def65
kebabkungen <94451739+kebabkungen@users.noreply.github.com>
2024-02-22 14:35:50 +0100

    tabs added

--- android/app/src/main/java/com/example/aida/MainActivity.kt
+++ android/app/src/main/java/com/example/aida/MainActivity.kt
@@ -12,8 +12,21 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.aida.ui.theme.AIDATheme
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.PrimaryTabRow
+import androidx.compose.material3.Tab
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.text.style.TextOverflow
+
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+
+        @OptIn(ExperimentalMaterial3Api::class)
+        override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             AIDATheme {
@@ -23,12 +36,30 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Greeting("Android")
+                    var state by remember { mutableStateOf(0) }
+                    val titles = listOf("Tab 1", "Tab 2", "Tab 3 with lots of text")
+                    Column {
+                        PrimaryTabRow(selectedTabIndex = state) {
+                            titles.forEachIndexed { index, title ->
+                                Tab(
+                                    selected = state == index,
+                                    onClick = { state = index },
+                                    text = { Text(text = title, maxLines = 2, overflow = TextOverflow.Ellipsis) }
+                                )
+                            }
+                        }
+                        Text(
+                            modifier = Modifier.align(Alignment.CenterHorizontally),
+                            text = "Text tab ${state + 1} selected",
+                            style = MaterialTheme.typography.bodyLarge
+                        )
+                    }
                 }
             }
         }
+
     }
 }
-
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
