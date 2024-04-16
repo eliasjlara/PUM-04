4e3105d4c718ce6ef417f65962db0e6b62370c40
alexanderortengren <aleorten9@gmail.com>
2024-04-16 16:51:12 +0200

    Sockets with image proof of concept

--- SocketExperiments/SocketCommunication/src/main/kotlin/Client.kt
+++ SocketExperiments/SocketCommunication/src/main/kotlin/Client.kt
@@ -2,28 +2,57 @@ package org.example
 import java.lang.Thread.sleep
 import java.net.Socket
 import java.nio.ByteBuffer
+import javax.imageio.ImageIO
+import javax.swing.JFrame
+import javax.swing.ImageIcon
+import javax.swing.JLabel
 
 class Client {
+    private fun displayImageFromByteArray(imageData: ByteArray) {
+        try {
+            // Read the image from the byte array
+            val image = ImageIO.read(imageData.inputStream())
+
+            // Create a JFrame to display the image
+            val frame = JFrame("Image Display")
+            frame.defaultCloseOperation = JFrame.EXIT_ON_CLOSE
+
+            // Create a JLabel to hold the image
+            val label = JLabel()
+            label.icon = ImageIcon(image)
+
+            // Add the JLabel to the frame
+            frame.contentPane.add(label)
+            frame.setSize(image.width, image.height)
+
+            // Pack the frame and set it visible
+            frame.pack()
+            frame.isVisible = true
+        } catch (ex: Exception) {
+            ex.printStackTrace()
+        }
+    }
     fun start() {
         val ip = "localhost"
         val port = 12345
         val socket = Socket(ip, port)
         println("Connected to server")
-        val frameBufferSize = ByteBuffer.allocate(4)
-        socket.getInputStream().read(frameBufferSize.array())
+        while (true){
+            val frameBufferSize = ByteBuffer.allocate(4)
+            socket.getInputStream().read(frameBufferSize.array())
 
-        val frameSize = frameBufferSize.int
+            val frameSize = frameBufferSize.int
 
-        //val frameBuffer = ByteArray(frameSize)
-        val frameBuffer = ByteBuffer.allocate(frameSize)
-        var bytesRead = 0
+            //val frameBuffer = ByteArray(frameSize)
+            val frameBuffer = ByteBuffer.allocate(frameSize)
+            var bytesRead = 0
 
-        while (bytesRead < frameSize){
-            bytesRead += socket.getInputStream().read(frameBuffer.array(), bytesRead, frameSize - bytesRead)
+            while (bytesRead < frameSize){
+                bytesRead += socket.getInputStream().read(frameBuffer.array(), bytesRead, frameSize - bytesRead)
+            }
+            displayImageFromByteArray(frameBuffer.array())
+
         }
-        val message = String(frameBuffer.array())
-        println("Received message: $message")
-
         socket.close()
     }
 }
--- SocketExperiments/SocketCommunication/src/main/kotlin/Server.kt
+++ SocketExperiments/SocketCommunication/src/main/kotlin/Server.kt
@@ -1,4 +1,5 @@
 package org.example
+import java.io.File
 import java.lang.Thread.sleep
 import java.net.ServerSocket
 import java.nio.ByteBuffer
@@ -11,12 +12,19 @@ class Server {
         val socket = serverSocket.accept()
         println("Client connected")
         sleep(1000)
-        val message = "Hello from the server!"
-        val frameSize = message.length
+        val path = "C:\\Users\\alexa\\IdeaProjects\\PUM-04\\SocketExperiments\\SocketCommunication\\src\\main\\kotlin\\socketbild.jpeg"
+        val file = File(path)
+        val message = file.readBytes()
+        val frameSize = message.size
         val frameBufferSize = ByteBuffer.allocate(4)
+
+        //val message = "Hello from the server!"
+        //val frameSize = message.length
+        //val frameBufferSize = ByteBuffer.allocate(4)
         frameBufferSize.putInt(frameSize)
         socket.getOutputStream().write(frameBufferSize.array())
-        socket.getOutputStream().write(message.toByteArray())
+        //socket.getOutputStream().write(message.toByteArray())
+        socket.getOutputStream().write(message)
         socket.close()
         serverSocket.close()
     }
