595ad8bbe16e189ed874556d9a471488b5e64763
Thea Antonson <thea.antonson@gmail.com>
2024-04-25 13:53:24 +0200

    added missing file from previous commit

---
+++ AIDA/ros2_humble_ws/src/image_recognition/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'image_recognition'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='user',
+    maintainer_email='user@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'video_transmit = image_recognition.video_transmit:main',
+            'video_receive = image_recognition.video_receive:main'
+        ],
+    },
+)
