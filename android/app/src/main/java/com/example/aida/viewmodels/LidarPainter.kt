a2e28d11a76d4d28ec1b2af038036044857e9fc2
alexanderortengren <159623570+alexanderortengren@users.noreply.github.com>
2024-05-22 08:49:40 +0200

    Created separate function for calculating the pixel value

--- android/app/src/main/java/com/example/aida/viewmodels/LidarPainter.kt
+++ android/app/src/main/java/com/example/aida/viewmodels/LidarPainter.kt
@@ -7,6 +7,9 @@ import android.graphics.Paint
 import kotlin.math.cos
 import kotlin.math.sin
 
+/**
+ * This class is used to create a LidarPainter object to convert Lidar data to Bitmap
+ */
 class LidarPainter(val width: Int, val height: Int, val maxLidarDistance: Int = 5000) {
 
     /**
@@ -18,26 +21,31 @@ class LidarPainter(val width: Int, val height: Int, val maxLidarDistance: Int = 
         val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.RGB_565)
         val canvas = Canvas(bitmap)
         canvas.drawColor(Color.WHITE)
-        val centerX = width / 2f
-        val centerY = height / 2f
-
-
         val paint = Paint()
         paint.color = Color.BLACK
         paint.strokeWidth = 3f
 
         for ((index, distance) in lidarValues.withIndex()) {
-            val angle = Math.toRadians(index.toDouble())
-            val normalizedDistanceX = distance.toFloat() / (maxLidarDistance/centerX)
-            val normalizedDistanceY = distance.toFloat() / (maxLidarDistance/centerY)
-
-            val x = centerX + normalizedDistanceX * cos(angle).toFloat()
-            val y = centerY + normalizedDistanceY * sin(angle).toFloat()
-
+            val (x, y) = calculatePixelValue(distance, index.toDouble())
+            // If pixel value is inside the bitmap, draw the point
             if (x >= 0 && x < width && y >= 0 && y < height) {
                 canvas.drawPoint(x, y, paint)
             }
         }
         return bitmap
+    }
+
+    /**
+     *  Calculate the pixel value for a given distance and angle
+     *  @param distance the distance to calculate the pixel value for
+     *  @param angleInDegrees the angle in degrees to calculate the pixel value for
+     */
+    private fun calculatePixelValue(distance: Int, angleInDegrees: Double): Pair<Float, Float>{
+        val angleInRadians = Math.toRadians(angleInDegrees)
+        val normalizedDistanceX = distance.toFloat() / (maxLidarDistance/width)
+        val normalizedDistanceY = distance.toFloat() / (maxLidarDistance/height)
+        val x = width / 2.0f + normalizedDistanceX * cos(angleInRadians).toFloat()
+        val y = height / 2.0f + normalizedDistanceY * sin(angleInRadians).toFloat()
+        return Pair(x, y)
     }
 }
